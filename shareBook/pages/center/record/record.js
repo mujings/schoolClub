4f97dbd2e5206f439728eb850ac7e831f92cb95d
黄阳富 <1951658+smallcookies@users.noreply.gitee.com>
2019-04-15 16:10:36 +0800

    no commit message

--- shareBook/pages/center/record/record.js
+++ shareBook/pages/center/record/record.js
@@ -1,66 +1,61 @@
-// pages/center/record/record.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    current: 'tab1',
+    tabs: [
+        {
+            key: 'tab1',
+            title: 'Tab 1',
+            content: 'Content of tab 1',
+        },
+        {
+            key: 'tab2',
+            title: 'Tab 2',
+            content: 'Content of tab 2',
+        },
+        {
+            key: 'tab3',
+            title: 'Tab 3',
+            content: 'Content of tab 3',
+        },
+    ],
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  onTabsChange(e) {
+    console.log('onTabsChange', e)
+    const {
+      key
+    } = e.detail
+    const index = this.data.tabs.map((n) => n.key).indexOf(key)
 
+    this.setData({
+      key,
+      index,
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+  onSwiperChange(e) {
+    console.log('onSwiperChange', e)
+    const {
+      current: index,
+      source
+    } = e.detail
+    const {
+      key
+    } = this.data.tabs[index]
 
+    if (!!source) {
+      this.setData({
+        key,
+        index,
+      })   
+    }
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
