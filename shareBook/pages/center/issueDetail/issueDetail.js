fdf68109d66da2a58f0baf7c0670df19a01d6790
暮竫 <374867413@qq.com>
2019-05-05 08:11:13 +0800

    0811

--- shareBook/pages/center/issueDetail/issueDetail.js
+++ shareBook/pages/center/issueDetail/issueDetail.js
@@ -1,66 +1,50 @@
-// pages/center/issueDetail/issueDetail.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  showImg: function (res) {
+    let that = this;
+    let index = res.currentTarget.id
+    wx.previewImage({
+      current: that.data.imgUrls[index], // 当前显示图片的http链接  
+      urls: that.data.imgUrls // 需要预览的图片http链接列表  
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  toUserDetail(e) {
+    wx.navigateTo({
+      url: '../userDetail/userDetail'
+    });
+  },
+  
+  toOrder(e) {
+    wx.navigateTo({
+      url: '../order/order'
+    });
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  toReport(e) {
+    wx.navigateTo({
+      url: '../reportBook/reportBook'
+    });
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
